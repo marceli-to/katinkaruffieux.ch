bcdb8a06c6b722af0ea09c0b698619f32fe0472e
Marcel Stadelmann <m@marceli.to>
2025-07-21 09:00:43 +0200

    wip

--- config/client.php
+++ config/client.php
@@ -1,5 +1,5 @@
 <?php
 return [
   'base_url' => env('APP_URL'),
-  'meta_description' => 'Katinka Ruffieux ist freiberufliche Lektorin mit Erfahrung in Politik und Kultur. Sie lebt bei Zürich und arbeitet u. a. für die Parlamentsdienste.'
+  'meta_description' => ''
 ];
